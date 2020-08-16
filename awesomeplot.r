75249ce3d1170843a3f7f9bea9de9842566b802b
oskar <oskar@austin.utexas.edu>
2020-08-16 14:40:23 -0500

    another awesome plot

--- awesomeplot.r
+++ awesomeplot.r
@@ -7,3 +7,9 @@ plot(rv2~rv1)
 abline(a = 0, b = 0, lty =3)
 
 # now we commit this to git... 
+
+# we should do that with ggplot, for comparison 
+# package  not loaded in this script 
+ggplot(data = data.frame(x=rv1, y=rv2), aes(x , y)) + 
+  geom_point() + geom_smooth(method = 'lm')
+  
